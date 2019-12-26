7e63827a344d9d08ef8b59087c7064b36d08351f
Juan Borrego <j.borrego@kostal.com>
2019-12-26 14:55:33 +0100

    file deletion

--- python_transfer_learning/inference.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import torchvision
-from torch import optim
-
-
-def load_model():
-    model_conv = torchvision.models.mobilenet_v2(pretrained=False)
-    optimizer = optim.SGD(*args, **kwargs)
-    model_conv.load_state_dict()
